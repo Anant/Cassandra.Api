202aad75667e32f84704174bb834ed77868c6f77
anomnaco <onanomnachi@gmail.com>
2020-04-21 11:24:29 -0400

    Data import changed to use JSON

--- astra.import/data/SolrToAstra.py
+++ astra.import/data/SolrToAstra.py
@@ -3,6 +3,7 @@ from cassandra.auth import PlainTextAuthProvider
 import json
 import requests
 from datetime import  datetime
+import time
 
 #Connect to Astra Cluster
 #Redo this after git project restructuring
@@ -26,11 +27,13 @@ session.set_keyspace('killrvideo')
 f = open('../../astra.import/schema/AstraTableDef')
 session.execute('CREATE TABLE IF NOT EXISTS '+str(f.read()))
 
+rows = 1000
+
 #Request data from solr
 params = (
     ('fl', '*'),
     ('q', '*'),
-    ('rows', '1000'),
+    ('rows', str(rows)),
 )
 
 response = requests.get('https://ss346483-us-east-1-aws.searchstax.com/solr/leaves_anant_stage/select', params=params)
@@ -89,57 +92,67 @@ for i in range(len(docs)):
         slugs_check = tmp_doc['slugs']
     except KeyError:
         tmp_doc['slugs'] = []
+		
+    try:
+		all_check = tmp_doc['all']
+    except KeyError:
+        tmp_doc['all'] = []
 
-    insert_query = session.prepare(
-        """
-        INSERT INTO killrvideo.leaves 
-        (is_archived, 
-        all, 
-        is_starred, 
-        user_name, 
-        user_email, 
-        user_id, 
-        tags, 
-        slugs, 
-        is_public, 
-        id, 
-        title, 
-        url, 
-        content_text, 
-        created_at, 
-        updated_at, 
-        mimetype, 
-        language, 
-        reading_time, 
-        domain_name, 
-        preview_picture, 
-        http_status, 
-        links, 
-        content)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""")
-    session.execute(insert_query, 
-        [
+    tmp_doc['is_public'] = str(tmp_doc['is_public'])
+    tmp_doc['user_id'] = str(tmp_doc['user_id'])
+    tmp_doc['http_status'] = str(tmp_doc['http_status'])
+    tmp_doc['tags'] = str(tmp_doc['tags'])
+    tmp_doc['slugs'] = str(tmp_doc['slugs'])
+    tmp_doc['all'] = str(tmp_doc['all'])
+    tmp_doc['links'] = str(tmp_doc['_links'])
+    
+    try:
+        del tmp_doc['_links']
+    except KeyError:
+        print("No _links key to delete")
+    
+    try:
+        del tmp_doc['published_by']
+    except KeyError:
+        #print("No published_by key to delete")
+        pass
+       
+    try:
+        del tmp_doc['published_at']
+    except KeyError:
+        #print("No published_at key to delete")
+        pass
+    
+    if i%(rows/10)==0:
+        print(str(i/(rows/100))+" % complete")
+    
+    json_doc = str(json.dumps(tmp_doc))
+    insert_query = session.execute(
+        "INSERT INTO killrvideo.leaves JSON %s" % "'"+json_doc.replace("'","''")+"'"
+        )
+    """insert_query = session.execute(
+        "INSERT INTO killrvideo.leaves(is_archived, all, is_starred, user_name, user_email, user_id, tags, slugs, is_public, id, title, url, content_text, created_at, updated_at, mimetype, language, reading_time, domain_name, preview_picture, http_status, links, content) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(
             tmp_doc['is_archived'],
-            str(tmp_doc['all']),
+            [tmp_doc['all']],
             tmp_doc['is_starred'],
-            str(tmp_doc['user_name']),
-            str(tmp_doc['user_email']),
-            str(tmp_doc['user_id']),
-            str(tmp_doc['tags']),
-            str(tmp_doc['slugs']),
-            str(tmp_doc['is_public']),
-            int(tmp_doc['id']),
-            tmp_doc['title'].encode('utf-8'),
-            str(tmp_doc['url']),
-            tmp_doc['content_text'].encode('utf-8'),
+            tmp_doc['user_name'],
+            tmp_doc['user_email'],
+            tmp_doc['user_id'],
+            tmp_doc['tags'],
+            tmp_doc['slugs'],
+            tmp_doc['is_public'],
+            tmp_doc['id'],
+            tmp_doc['title'],
+            tmp_doc['url'],
+            tmp_doc['content_text'],
             datetime.strptime(tmp_doc['created_at'],'%Y-%m-%dT%H:%M:%SZ'),
             datetime.strptime(tmp_doc['updated_at'],'%Y-%m-%dT%H:%M:%SZ'),
-            str(tmp_doc['mimetype']),
-            str(tmp_doc['language']),
-            int(tmp_doc['reading_time']),
-            str(tmp_doc['domain_name']),
-            tmp_doc['preview_picture'].encode('utf-8'),
-            str(tmp_doc['http_status']),
-            str(tmp_doc['_links']),
-            tmp_doc['content'].encode('utf-8'),
-        ])
+            tmp_doc['mimetype'],
+            tmp_doc['language'],
+            tmp_doc['reading_time'],
+            tmp_doc['domain_name'],
+            tmp_doc['preview_picture'],
+            tmp_doc['http_status'],
+            tmp_doc['_links'],
+            tmp_doc['content'],
+        ))"""
