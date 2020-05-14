e3e0d1a999f27b1d63d0f9cba4595fc936c53836
Arpan Patel <adp8ke@gmail.com>
2020-05-14 16:30:07 -0400

    flushed out post and patch requests, and now awaiting testing once more information is obtained regarding what the request body requires

--- astra.api/leaves.api.node/src/Routes/router.js
+++ astra.api/leaves.api.node/src/Routes/router.js
@@ -6,36 +6,36 @@ const leavesRouter = express.Router();
 // const jsonParser = express.json();
 const cassandra = require('cassandra-driver');
 const config = require('../config');
-const ExpressCassandra = require('express-cassandra');
+// const ExpressCassandra = require('express-cassandra');
 //leavesModel that contains the model for the table
-const leavesModel = require('../Models/leavesModel');
+// const leavesModel = require('../Models/leavesModel');
 
-const models = ExpressCassandra.createClient({
-  clientOptions: {
-    cloud: { secureConnectBundle: `../../astra.credentials/secure-connect-${config.ASTRA_CLUSTER}.zip`},
-    credentials: { username: config.ASTRA_USERNAME, password: config.ASTRA_PASSWORD },
-    keyspace: `${config.ASTRA_KEYSPACE}`
-  }
+// const models = ExpressCassandra.createClient({
+//   clientOptions: {
+//     cloud: { secureConnectBundle: `../../astra.credentials/secure-connect-${config.ASTRA_CLUSTER}.zip`},
+//     credentials: { username: config.ASTRA_USERNAME, password: config.ASTRA_PASSWORD },
+//     keyspace: `${config.ASTRA_KEYSPACE}`
+//   }
+// });
+
+// const MyModel = models.loadSchema('Leaves', leavesModel);
+
+// // MyModel or models.instance.Leaves can now be used as the model instance
+// console.log(models.instance.Leaves === MyModel);
+
+
+
+// set up connection to Astra using cassandra-driver from DataStax
+const client = new cassandra.Client({
+  cloud: { secureConnectBundle: `../../astra.credentials/secure-connect-${config.ASTRA_CLUSTER}.zip`},
+  credentials: { username: config.ASTRA_USERNAME, password: config.ASTRA_PASSWORD }
 });
 
-const MyModel = models.loadSchema('Leaves', leavesModel);
-
-// MyModel or models.instance.Leaves can now be used as the model instance
-console.log(models.instance.Leaves === MyModel);
-
-
-
-//set up connection to Astra using cassandra-driver from DataStax
-// const client = new cassandra.Client({
-//   cloud: { secureConnectBundle: `../../astra.credentials/secure-connect-${config.ASTRA_CLUSTER}.zip`},
-//   credentials: { username: config.ASTRA_USERNAME, password: config.ASTRA_PASSWORD }
-// });
-
-//dont actually need this to run the node api, but we can keep it for confirmation of connection
-//Connecting to Astra database with console.log confirming connection
-// client.connect(function(err, result){
-//   console.log('astra connected');
-// });
+// dont actually need this to run the node api, but we can keep it for confirmation of connection
+// Connecting to Astra database with console.log confirming connection
+client.connect(function(err, result){
+  console.log('astra connected');
+});
 
 //Using express router and the base route of /api/leaves/
 leavesRouter
@@ -44,23 +44,25 @@ leavesRouter
   .get( async (req, res, next) => {
     try{
 
-      // //query to make to Astra 
-      // let query = `SELECT * FROM ${config.ASTRA_KEYSPACE}.${config.ASTRA_TABLE};`;
+      //query to make to Astra 
+      let query = `SELECT * FROM ${config.ASTRA_KEYSPACE}.${config.ASTRA_TABLE};`;
 
-      // //await client executing the query and set the results into a variable
-      // let result = await client.execute(query);
+      //await client executing the query and set the results into a variable
+      let result = await client.execute(query);
 
-      // //return the result of the query in JSON format
-      // return res.status(200).json(result.rows);
+      //return the result of the query in JSON format
+      return res.status(200).json(result.rows);
 
-      models.instance.Leaves.findOne({id: 13952}, function(err, result){
-        if(err){
-          console.log(err);
-          return;
-        }
 
-        console.log(result);
-      })
+      // express-cassandra find one
+      // models.instance.Leaves.findOne({id: 13952}, function(err, result){
+      //   if(err){
+      //     console.log(err);
+      //     return;
+      //   }
+
+      //   console.log(result);
+      // })
 
     }
 
@@ -70,58 +72,74 @@ leavesRouter
       });
       next();
     }
-  });
+  })
 
   //Currently under construction and subject to change
-  // .post(jsonParser, async (req, res, next) => {
-  //   try{
+  .post(jsonParser, async (req, res, next) => {
+    try{
 
-  //     const {is_archived, is_starred, user_name, user_email, user_id, tags, is_public,
-  //       id, uid, title, url, content, created_at, updated_at, published_at, published_by,
-  //       starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture,
-  //       http_status, headers, origin_url, _links } 
-  //       = req.body;
+      //object destructuring of request body  
+      const { is_archived, is_starred, user_name, user_email, user_id, tags, is_public,
+        id, uid, title, url, content, created_at, updated_at, published_at, published_by,
+        starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture,
+        http_status, headers, origin_url, _links } 
+        = req.body;
 
-  //     newLeaf = {is_archived, is_starred, user_name, user_email, user_id, tags, is_public,
-  //       id, uid, title, url, content, created_at, updated_at, published_at, published_by,
-  //       starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture,
-  //       http_status, headers, origin_url, _links };
+      //creating new object to insert into Astra
+      newLeaf = { is_archived, is_starred, user_name, user_email, user_id, tags, is_public,
+        id, uid, title, url, content, created_at, updated_at, published_at, published_by,
+        starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture,
+        http_status, headers, origin_url, _links };
 
-  //     for (const [key, value] of Object.entries(newLeaf))
-  //       if (!value)
-  //         return res.status(400).json({
-  //           error: `Missing '${key}' in request body`
-  //         });
+      //validate for missing keys in request body
+      for (const [key, value] of Object.entries(newLeaf))
+        if (!value)
+          return res.status(400).json({
+            error: `Missing '${key}' in request body`
+          });
 
-  //     let query = `INSERT INTO ${config.ASTRA_KEYSPACE}.${config.ASTRA_TABLE}(is_archived, is_starred, user_name, user_email, user_id, tags, is_public, id, uid, title, url, content, created_at, updated_at, published_at, published_by, starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture, http_status, headers, origin_url, _links) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);`; 
+      //old query
+      // let query = `INSERT INTO ${config.ASTRA_KEYSPACE}.${config.ASTRA_TABLE}(is_archived, is_starred, user_name, user_email, user_id, tags, is_public, id, uid, title, url, content, created_at, updated_at, published_at, published_by, starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture, http_status, headers, origin_url, _links) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);`; 
 
-  //     let params = [is_archived, is_starred, user_name, user_email, user_id, tags, is_public,
-  //       id, uid, title, url, content, created_at, updated_at, published_at, published_by,
-  //       starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture,
-  //       http_status, headers, origin_url, _links];
+      // old params
+      // let params = [is_archived, is_starred, user_name, user_email, user_id, tags, is_public,
+      //   id, uid, title, url, content, created_at, updated_at, published_at, published_by,
+      //   starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture,
+      //   http_status, headers, origin_url, _links];
 
-  //     client.execute(query, params, function(err, result){
-  //       if(err){
-  //       //   console.log(err);
-  //         return res.status(400).json({
-  //           error: 'error adding'
-  //         });
-  //       }
-  //       else{
-  //         console.log(result);
-  //         return res.status(201).json({
-  //           message: 'created'
-  //         });
-  //       }
-  //     });
-  //   }
-  //   catch(e){
-  //     res.status(400).json({
-  //       message: e.message
-  //     });
-  //     next();
-  //   }
-  // });
+      // old client execute 
+      // client.execute(query, params, function(err, result){
+      //   if(err){
+      //   //   console.log(err);
+      //     return res.status(400).json({
+      //       error: 'error adding'
+      //     });
+      //   }
+      //   else{
+      //     console.log(result);
+      //     return res.status(201).json({
+      //       message: 'created'
+      //     });
+      //   }
+      // });
+
+      //query to insert into Astra
+      let query = `INSERT INTO ${config.ASTRA_KEYSPACE}.${config.ASTRA_TABLE} JSON ?;`; 
+
+      //execute query to Astra and store result in variable
+      let result = await client.execute(query, [JSON.stringify(newLeaf)], { prepare : true });
+
+      //send back the result to the client
+      return res.status(201).json(result);
+
+    }
+    catch(e){
+      res.status(400).json({
+        message: e.message
+      });
+      next();
+    }
+  });
 
 
 //Using express router and the route of /api/leaves/:id
@@ -161,60 +179,73 @@ leavesRouter
     }
   })
 
-  //Currently under construction and subject to change
-  // .patch(jsonParser, async (req, res, next) => {
-  //   try{
+  // Currently under construction and subject to change
+  .patch(jsonParser, async (req, res, next) => {
+    try{
       
-  //     let leafId = req.params.leafId;
+      //extract id from params
+      let leafId = req.params.leafId;
 
-  //     const {is_archived, is_starred, user_name, user_email, user_id, tags, is_public, 
-  //       id, uid, title, url, content, created_at, updated_at, published_at, published_by,
-  //       starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture,
-  //       http_status, headers, origin_url, _links } 
-  //       = req.body;
+      //object destructuring request body
+      const { is_archived, is_starred, user_name, user_email, user_id, tags, is_public, 
+        id, uid, title, url, content, created_at, updated_at, published_at, published_by,
+        starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture,
+        http_status, headers, origin_url, _links } 
+        = req.body;
 
-  //     newLeaf = {is_archived, is_starred, user_name, user_email, user_id, tags, is_public,
-  //       id, uid, title, url, content, created_at, updated_at, published_at, published_by,
-  //       starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture,
-  //       http_status, headers, origin_url, _links };
+      //creating new object to insert into Astra
+      newLeaf = { is_archived, is_starred, user_name, user_email, user_id, tags, is_public,
+        id, uid, title, url, content, created_at, updated_at, published_at, published_by,
+        starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture,
+        http_status, headers, origin_url, _links };
 
-  //     for (const [key, value] of Object.entries(newLeaf))
-  //       if (!value)
-  //         return res.status(400).json({
-  //           error: `Missing '${key}' in request body`
-  //         });
+      //validate for missing keys in request body
+      for (const [key, value] of Object.entries(newLeaf))
+        if (!value)
+          return res.status(400).json({
+            error: `Missing '${key}' in request body`
+          });
 
-  //     //udpate query depending on what we allow for being updated
-  //     let query = `UPDATE ${config.ASTRA_KEYSPACE}.${config.ASTRA_TABLE} SET email=?, firstname=?, lastname=? WHERE id=?;`;
 
-  //     //change out params depending on what we allow for being updated
-  //     const params = [is_archived, is_starred, user_name, user_email, user_id, tags, is_public,
-  //       id, uid, title, url, content, created_at, updated_at, published_at, published_by,
-  //       starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture,
-  //       http_status, headers, origin_url, _links, leafId];
+      //old method using params
+      //change out params depending on what we allow for being updated
+      // const params = [is_archived, is_starred, user_name, user_email, user_id, tags, is_public,
+      //   id, uid, title, url, content, created_at, updated_at, published_at, published_by,
+      //   starred_at, annotations, mimetype, language, reading_time, domain_name, preview_picture,
+      //   http_status, headers, origin_url, _links, leafId];
 
-  //     client.execute(query, params, function(err, result){
-  //       if(err){
-  //       //   console.log(err);
-  //         res.status(400).json({
-  //           error: 'error updating'
-  //         });
-  //       }
-  //       else{
-  //         console.log(result);
-  //         res.status(200).json({
-  //           message: 'updated',
-  //         });
-  //       }
-  //     });
-  //   }
-  //   catch(e){
-  //     res.status(400).json({
-  //       message: e.message
-  //     });
-  //     next();
-  //   }
-  // })
+      // client.execute(query, params, function(err, result){
+      //   if(err){
+      //   //   console.log(err);
+      //     res.status(400).json({
+      //       error: 'error updating'
+      //     });
+      //   }
+      //   else{
+      //     console.log(result);
+      //     res.status(200).json({
+      //       message: 'updated',
+      //     });
+      //   }
+      // });
+
+
+      //udpate query depending on what we allow for being updated
+      let query = `INSERT INTO ${config.ASTRA_KEYSPACE}.${config.ASTRA_TABLE} JSON ? DEFAULT UNSET;`; 
+
+      //execute query to Astra and store result in variable
+      let result = await client.execute(query, [JSON.stringify(newLeaf)], { prepare : true });
+
+      //send back the result to the client
+      return res.status(201).json(result);
+    }
+    catch(e){
+      res.status(400).json({
+        message: e.message
+      });
+      next();
+    }
+  })
 
   //DELETE request to /api/leaves/:id
   .delete( async (req, res, next) => {
