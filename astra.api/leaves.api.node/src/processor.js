cbdcc4fbcfb33adc1b1a4b49e9b93bb3c75076c4
Arpan Patel <adp8ke@gmail.com>
2020-05-18 17:42:25 -0400

    after lots of trial and error and debugging, post is working. Used articler-parser package to get preview_picture, title, content, and content_text with regex from content. But yeah, post is working to astra and all columns are filled when visualized in astra workbook with select * query. Still need further instruction as for what to do with id generation

--- astra.api/leaves.api.node/src/processor.js
+++ astra.api/leaves.api.node/src/processor.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { extract } = require('article-parser');
 
 const processor = async(newLeaf) => {
 
-  newLeaf.id = 123456781;
+  newLeaf.id = 1234212;
   newLeaf.is_archived = 1;
   newLeaf.is_starred = 0;
   newLeaf.user_name = 'admin';
@@ -16,12 +17,6 @@ const processor = async(newLeaf) => {
   newLeaf.links = [`api/entries/${newLeaf.id}`];
   newLeaf.slugs = newLeaf.tags;
 
-  newLeaf.content = '';
-  newLeaf.content_text = '';
-  newLeaf.reading_time = 1;
-  newLeaf.preview_picture = '';
-
-
   //use axios to make a get request to the url
   await axios.get(`${newLeaf.url}`)
     .then(res => {
@@ -32,18 +27,33 @@ const processor = async(newLeaf) => {
       //set newLeaf.http_status = status code converted to string
       newLeaf.http_status = res.status.toString();
 
-      //load htmol document and use $ to mimic jQuery
+      //load html into cheerio and use $ to mimic jQuery
       let $ = cheerio.load(res.data);
- 
-      //find title and assign it to newLeaf.title
-      newLeaf.title = $('title').text();
 
       //find language attribute and assign it to newLeaf.language
       newLeaf.language = $('html').attr('lang');
       
-
     });
 
+  //await article parser method extract parsing url 
+  const article = await extract(newLeaf.url);
+
+  //set preview_picture equal to image returned in article object
+  newLeaf.preview_picture = article.image;
+
+  //set content equal to readable content returned in article object
+  newLeaf.content = article.content;
+
+  //set title equal to title returned in article object
+  newLeaf.title = article.title;
+
+  //set content_text equal to content that is stripped of html elements with regex
+  newLeaf.content_text = newLeaf.content.replace(/<[^>]+>/g, '');
+
+  //generate reading_time in minutes from article object (convert from seconds)
+  newLeaf.reading_time = Math.round(article.ttr / 60);
+
+  //return newLeaf to use in router.js
   return newLeaf;
 
 };
