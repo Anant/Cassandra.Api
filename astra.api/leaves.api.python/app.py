eb407fada7e2594d98db81aeb541ef3620844c04
Obioma Anomnachi <onanomnachi@gmail.com>
2020-05-21 17:23:45 +0000

    Schema Changes and Python Processor

--- astra.api/leaves.api.python/app.py
+++ astra.api/leaves.api.python/app.py
@@ -4,6 +4,53 @@ import json
 import requests
 from datetime import  datetime
 from flask import Flask, jsonify, request, abort
+import hashlib
+from bs4 import BeautifulSoup
+import re
+import readtime
+
+def processURL(url):
+    page = requests.get(url)
+    mimetype = page.headers['content-type']
+    http_status = str(page.status_code)
+    
+    id = hashlib.md5(url.encode()).hexdigest()
+    is_archived = 1
+    is_starred = 0
+    user_name = 'admin'
+    user_email = 'rahul@example.com'
+    user_id = 1
+    is_public = False
+    domain_name = re.search('https?:\/\/[^#?\/]+/',url).group(0)
+    domain_name = re.compile(r"https?://(www\.)?").sub('', domain_name).strip().strip('/')
+    created_at = datetime.now()
+    updated_at = datetime.now()
+    links = ['api/entries/%s',id]
+    tags = []
+    slugs = tags
+
+    bs = BeautifulSoup(page.content, 'html.parser')
+    images = bs.find_all('img', {'src':re.compile('.jpg')})
+    title = bs.title.string
+    if images == []:
+        preview_picture = 'https://dummyimage.com/170/000/ffffff&text='+(title.replace(' ','%20'))
+    else:
+        preview_picture = images[0]['src']
+    language = bs.lang
+    if language == None:
+        language = 'en'
+    content = str(bs)
+    content_text = bs.text
+    reading_time = readtime.of_html(str(bs))
+    result = {'is_archived':is_archived, 'is_starred':is_starred, 'user_name':user_name,'user_email':user_email, 'user_id':user_id, 'tags':tags, 'slugs':slugs,
+    'is_public':is_public, 'id':id, 'title':title, 'url':url, 'content_text':content_text, 'created_at':created_at, 'updated_at':updated_at, 'mimetype':mimetype,
+    'language':language, 'reading_time':reading_time, 'domain_name':domain_name, 'preview_picture':preview_picture, 'http_status':http_status, 'links':links, 
+    'content':content, 'id':id}
+    all = list(result.values())
+    all = [str(i) for i in all]
+    result['all'] = all
+    return result
+
 
 #Connect to Astra Cluster
 #Redo this after git project restructuring
@@ -73,5 +120,17 @@ def delById(id):
     else:
         return jsonify(result)
 
+@app.route('/api/leaves',methods=['POST'])
+def pushRow():
+    req_data = request.get_json()
+    doc = str(json.dumps(processURL(req_data['url'])))
+    rows = session.execute("INSERT INTO "+cred['keyspace']+'.'+cred['table']+" JSON %s" % "'"+doc.replace("'","''")+"'")
+    #print(type(rows))
+    result = []
+    for row in rows:
+        #print(type(str(row)))
+        result.append(json.loads(row.json))
+    return jsonify(result)
 
+#print(processURL('https://github.com/Anant/cassandra.api'))
 app.run(port=8000,debug=True)
